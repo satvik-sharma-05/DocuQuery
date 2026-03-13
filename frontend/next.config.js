9fb3b77caaaed78c46a4fb0698d9f51dbee56bb4
Satvik Sharma <sharmasatvik031@gmail.com>
2026-03-13 21:00:42 +0530

    Add explicit webpack alias configuration for @ path

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // App directory is enabled by default in Next.js 14
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname),
+        }
+        return config
+    },
 }
 
 module.exports = nextConfig
